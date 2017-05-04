0de46908fde9dff16f6b520ff39e5d5d7546fc08
Menezes <erich.menezes@hp.com>
2017-05-04 20:39:03 -0300

    adding fizzbuzz function

--- FizzBuzz.js
+++ FizzBuzz.js
@@ -2,4 +2,21 @@ const isDivisibleByN = (dividend, divisor) => {
   return dividend % divisor === 0;
 }
 
-module.exports = isDivisibleByN;
+const fizzBuzzFunction = () => {
+  const arr = [];
+
+  for (let i = 1; i <= 100; i++) {
+    let str = '';
+    if (isDivisibleByN(i, 3)) {
+      str += 'Fizz'
+    }
+    if (isDivisibleByN(i, 5)) {
+      str += 'Buzz'
+    }
+    arr[i] = str || i;
+  }
+
+  return arr;
+}
+
+module.exports = {isDivisibleByN, fizzBuzzFunction};
--- test/FIzzBuzzTest.js
+++ test/FIzzBuzzTest.js
@@ -1,5 +1,6 @@
 const assert = require('assert');
 const isDivisibleByN = require('../FizzBuzz').isDivisibleByN;
+const fizzBuzzFunction = require('../FizzBuzz').fizzBuzzFunction;
 
 describe('Function returns true if dividend is divisible by divisor', function () {
   context('When rest is 0', function () {
@@ -13,6 +14,38 @@ describe('Function returns true if dividend is divisible by divisor', function (
     it('it should return false', function () {
       assert.equal(false, isDivisibleByN(5, 3));
       assert.equal(false, isDivisibleByN(31, 5));
+    });
+  });
+});
+
+describe('Function returns a string if number is divisible by 3 or 5', function () {
+  let arr;
+
+  before(function() {
+    arr = fizzBuzzFunction();
+  });
+
+  context('When number is divisible by 3', function () {
+    it('it should return Fizz', function () {
+      assert.equal('Fizz', arr[3]);
+      assert.equal('Fizz', arr[9]);
+      assert.equal('Fizz', arr[33]);
+    });
+  });
+
+  context('When number is divisible by 5', function () {
+    it('it should return Buzz', function () {
+      assert.equal('Buzz', arr[5]);
+      assert.equal('Buzz', arr[10]);
+      assert.equal('Buzz', arr[20]);
+    });
+  });
+
+  context('When number is divisible by 3 and 5', function () {
+    it('it should return FizzBuzz', function () {
+      assert.equal('FizzBuzz', arr[15]);
+      assert.equal('FizzBuzz', arr[30]);
+      assert.equal('FizzBuzz', arr[45]);
     });
   });
 });
